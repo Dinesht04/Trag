b664dac9dcaf88b956fe3983f6ae404c2040bbbe
Dinesht04 <dineshtyagi567@gmail.com>
2025-08-24 00:27:23 +0530

    project slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,12 @@
 import FlipCardsSection from "@/components/flip-card-section"
+import { ProjectSlider } from "@/components/project-slider"
 
 export default function Home() {
   return (
     <div className="min-h-screen py-10">
       
-      {/* <div className="">
-        <FlipCardsSection />
-      </div> */}
+      <ProjectSlider/>
+
     </div>
   )
 }
