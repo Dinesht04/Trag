5c7f8c71a2acde2c527c16267cb9258357a371bf
Pranshu Raj <rajpranshu2222@gmail.com>
2025-08-26 16:22:09 +0530

    removed page.tsx entries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,7 @@
-import ProjectsDoneSection from "@/components/how-to-component"
+
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-10">
-
-
-      <ProjectsDoneSection />
-     
-    </div>
+    <></>
   )
 }
