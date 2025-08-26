6d21cbfe9e38e801aa26786a7f214a9927e7dcce
Pranshu Raj <rajpranshu2222@gmail.com>
2025-08-26 15:17:27 +0530

    center hero component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,10 @@ import HomePage from "@/components/hero-component"
 
 export default function Home() {
   return (
-    <div className="min-h-screen py-10">
+    <div className="absolute -z-10 inset-0 h-full w-full 
+bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] 
+bg-[size:20px_20px] min-h-screen bg-gray-50 "
+   >
 
 
       <HomePage />
--- src/components/hero-component.tsx
+++ src/components/hero-component.tsx
@@ -51,9 +51,7 @@ export default function HomePage() {
 
   return (
     <main
-      className=" absolute -z-10 inset-0 h-full w-full 
-bg-[linear-gradient(to_right,#73737320_1px,transparent_1px),linear-gradient(to_bottom,#73737320_1px,transparent_1px)] 
-bg-[size:20px_20px] min-h-screen bg-gray-50 flex flex-col items-center justify-center px-4 py-8"
+      className="  flex flex-col items-center justify-center px-4 py-8"
     >
       {/* Available Status */}
       <motion.div
