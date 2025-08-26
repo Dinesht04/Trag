ea855ae6c3ef756ea90eb7a8b19d55c3c6feb859
Pranshu Raj <rajpranshu2222@gmail.com>
2025-08-26 16:34:32 +0530

    small tweaks in how to component and added to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 import { ChatAccordion } from "@/components/chat-accordian"
 import { ComparisonTable } from "@/components/comparison-table"
 import HomePage from "@/components/hero-component"
+import ProjectsDoneSection from "@/components/how-to-component"
 import { MotivationalQuote } from "@/components/motivational-quote"
 import Navbar from "@/components/navbar"
 
@@ -27,6 +28,8 @@ export default function Home() {
       <div className="py-20">
         <ComparisonTable/>
       </div>
+
+      <ProjectsDoneSection/>
 
       
      
